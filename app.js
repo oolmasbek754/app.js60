83456073573394879e5c4463279a9f4130cad239
Odilbekov O`lmasbek <oolmasbek754@gmail.com>
2026-04-16 16:35:56 +0500

    Add commented code for toList function

---
+++ app.js
@@ -0,0 +1,16 @@
+//14-usul
+//const list = document.querySelector("ol");
+
+//Array.prototype.toList = function () {
+//  const fragment = document.createDocumentFragment();
+
+//  this.forEach(text => {
+//    const li = document.createElement("li");
+//    li.textContent = text;
+//    fragment.appendChild(li);
+//  });
+
+//  list.appendChild(fragment);
+//};
+
+//new Array(1000).fill("hiii").toList();
