70dc295ebaca17571fbf74f4bfebb9b429775cae
Odilbekov O`lmasbek <oolmasbek754@gmail.com>
2026-04-16 16:37:44 +0500

    Comment out code snippets in app.js

--- app.js
+++ app.js
@@ -14,3 +14,36 @@
 //};
 
 //new Array(1000).fill("hiii").toList();
+
+//15-usul
+//const list = document.querySelector("ol");
+//const template = document.createElement("template");
+
+//for (let i = 0; i < 1000; i++) {
+//  template.innerHTML = `<li>hiii</li>`;
+//  list.appendChild(template.content.cloneNode(true));
+//}
+
+//16-usul
+//<ol id="list"></ol>
+
+//<style>
+//  #list {
+//    counter-reset: item;
+//  }
+
+//  #list li::before {
+ //   counter-increment: item;
+//    content: counter(item) ". ";
+//  }
+//</style>
+
+//<script>
+//  const list = document.querySelector("#list");
+
+//  let i = 0;
+//  while (i < 1000) {
+ //   list.insertAdjacentHTML("beforeend", "<li>hiii</li>");
+ //   i++;
+ // }
+//</script>
